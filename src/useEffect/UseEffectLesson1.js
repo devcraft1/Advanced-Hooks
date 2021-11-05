333b10d051015d717709b7c5082f96ac24943dfd
devcraft1 <ukonudennis64@gmail.com>
2021-11-05 19:08:43 +0100

    useEffect

--- src/useEffect/UseEffectLesson1.js
+++ src/useEffect/UseEffectLesson1.js
@@ -1,9 +1,24 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 
 function UseEffectLesson1() {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        fetch(`https://api.github.com/users`)
+            .then((response) => response.json())
+            .then(setData)
+            .catch((e) => e)
+    }, [])
     return (
         <div>
-            <h3>Lesson 1</h3>
+            <ul>
+                {data.map((user) =>
+                    <li key={user.id}>{user.login}</li>
+                )}
+            </ul>
+            <button onClick={() => setData([])}>
+                Clear data
+            </button>
         </div>
     );
 }
