6c15dde5d9d2e5a2577a4c72ca3552f0ccd81228
devcraft1 <ukonudennis64@gmail.com>
2021-11-06 23:35:56 -0800

    fetchedData

--- src/customHook/UseFetch.js
+++ src/customHook/UseFetch.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import { Fetch } from './Fetch';
+import React from 'react'
+import { Fetch } from './Fetch'
 
 function UseFetch({ login }) {
-    const { loading, data, error } = Fetch('https://hn.algolia.com/api/v1/search?query=redux');
+    const { loading, data, error } = Fetch(`https://api.github.com/users/${login}`);
     if (loading) return <h1>loading...</h1>
     if (error) return (
         <pre>{JSON.stringify(error, null, 2)}</pre>
     )
     return (
         <div>
-            {/* test data  */}
             <pre>{JSON.stringify(data, null, 2)}</pre>
         </div>
     )
