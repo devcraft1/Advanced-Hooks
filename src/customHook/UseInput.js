4e7abf28c5b492e547a9cd0d0a56242da8d59cba
devcraft1 <ukonudennis64@gmail.com>
2021-11-06 23:59:43 -0800

    fetchedData

--- src/customHook/UseInput.js
+++ src/customHook/UseInput.js
@@ -3,10 +3,40 @@ import { Input } from './Input';
 
 
 function UseInput() {
-    const { } = Input()
 
+    const [sound, setSound] = useState('')
+    const [color, setColor] = useState('#000000')
+
+    const submit = (e) => {
+        e.preventDefault();
+        alert(`${sound} sounds like ${color}`)
+        setColor('#000000')
+        setSound('')
+    }
     return (
-        <div></div>)
+        <div>
+            <form onSubmit={submit}>
+                <input type='text'
+                    value={sound}
+                    placeholder='sound...'
+                    onChange={(e) => {
+                        setSound(e.target.value)
+                    }}
+                />
 
+                <input type='color'
+                    value={color}
+                    onChange={(e) => {
+                        setColor(e.target.value)
+                    }}
+                />
+                <button>ADD</button>
+            </form>
+        </div>
+    );
 }
-export default UseInput
+
+  
+
+
+}  export default UseStateLesson4
